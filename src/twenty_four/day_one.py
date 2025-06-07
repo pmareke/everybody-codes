b3c4d939408731b4d59085f9d58762114946c2f9
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-06-07 11:21:04 +0200

    feat: solve part two day one 2024

--- src/twenty_four/day_one.py
+++ src/twenty_four/day_one.py
@@ -1,12 +1,28 @@
 class DayOne:
-    _POINTS = {"A": 0, "B": 1, "C": 3}
+    _POINTS = {
+        "x": 0,
+        "A": 0,
+        "B": 1,
+        "C": 3,
+        "D": 5,
+    }
 
     def __init__(self, filename: str) -> None:
         self.filename = filename
-
-    def solve(self) -> int:
         with open(self.filename) as file:
             lines = file.readlines()
             lines = list(map(lambda x: x.strip(), lines))
+            self.line = lines[0]
 
-        return sum([self._POINTS[item] for item in lines[0]])
+    def part_one(self) -> int:
+        return sum([self._POINTS[item] for item in self.line])
+
+    def part_two(self) -> int:
+        sum = 0
+        for idx in range(0, len(self.line), 2):
+            x = self.line[idx]
+            y = self.line[idx + 1]
+            sum += self._POINTS[x] + self._POINTS[y]
+            if x != "x" and y != "x":
+                sum += 2
+        return sum
--- tests/twenty_four/test_day_one.py
+++ tests/twenty_four/test_day_one.py
@@ -8,13 +8,27 @@ class TestDay01:
     @pytest.mark.parametrize(
         "filename, expected",
         [
-            ("inputs/2024/01.example", 5),
-            ("inputs/2024/01.in", 1289),
+            ("inputs/2024/01_01.example", 5),
+            ("inputs/2024/01_01.in", 1289),
         ],
     )
-    def test_solve_problem(self, filename: str, expected: int) -> None:
+    def test_solve_part_one(self, filename: str, expected: int) -> None:
         day_one = DayOne(filename)
 
-        result = day_one.solve()
+        result = day_one.part_one()
+
+        expect(result).to(equal(expected))
+
+    @pytest.mark.parametrize(
+        "filename, expected",
+        [
+            ("inputs/2024/01_02.example", 28),
+            ("inputs/2024/01_02.in", 5593),
+        ],
+    )
+    def test_solve_part_two(self, filename: str, expected: int) -> None:
+        day_one = DayOne(filename)
+
+        result = day_one.part_two()
 
         expect(result).to(equal(expected))
